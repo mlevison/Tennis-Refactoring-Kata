af82c367c49a50a64cfa0b848a69f7993150db48
zsoldaat <zac.soldaat@gmail.com>
2024-10-15 16:02:49 -0400

    refactor score to string logic

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -14,6 +14,19 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
+    private String getTextScoreFromNumber(int score) {
+        if (score==0)
+            return "Love";
+        if (score==1)
+            return "Fifteen";
+        if (score==2)
+            return "Thirty";
+        if (score==3)
+            return "Forty";
+
+        throw new IllegalStateException();
+    }
+
     public String getScore(){
         String score = "";
 
@@ -52,60 +65,11 @@ public class TennisGame2 implements TennisGame
             return score;
         }
 
+        P1res = getTextScoreFromNumber(P1point);
+        P2res = getTextScoreFromNumber(P2point);
+
+        return P1res + "-" + P2res;
         
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        
-       
-        return score;
     }
     
     public void SetP1Score(int number){
